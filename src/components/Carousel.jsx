4215a64438047dac7681849a2cd7e02ef42a6334
Akash9698 <senthilsenthil98832@gmail.com>
2024-03-07 17:53:57 +0530

    done

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,13 +1,44 @@
 import React, { Component } from "react";
-import "./Carousel.css";
 import { images } from "../data/CarouselData";
-// you can explore more - and check as how to use materiul ui
-import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos'
-import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
+import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
+import './Carousel.css'
+import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
 
-// implement the class below
 class Carousel extends Component {
-  
+
+    constructor() {
+        super()
+
+        this.state = {
+            count: 0
+        }
+    }
+
+    handleClick = (operation) => {
+        operation === "left" ? this.state.count !== 0 ? this.setState({ count: this.state.count - 1 }) : this.setState({ count: images.length - 1 }) : this.state.count !== images.length - 1 ?
+            this.setState({ count: this.state.count + 1 }) : this.setState({ count: 0 })
+    }
+
+    render() {
+
+        return (
+            <div id="carousel">
+                <div id="innerCarousel" style={{ backgroundImage: `url(${images[this.state.count].img})` }}>
+
+                    <div id="leftButton" onClick={() => this.handleClick("left")}>
+                        <ArrowBackIosIcon style={{ color: "white", fontSize: "30px" }} />
+                    </div>
+                    <div id="centerText">
+                        <h1>{images[this.state.count].title}</h1>
+                        <p>{images[this.state.count].subtitle}</p>
+                    </div>
+                    <div id="rightbutton" onClick={() => this.handleClick("right")}>
+                        <ArrowForwardIosIcon style={{ color: "white", fontSize: "30px"}} />
+                    </div>
+                </div>
+            </div>
+        )
+    }
 }
 
 export default Carousel;
